a17e8069fa1270c65e78364e00b34bbc14726fe0
omipapal12 <omipapal12@gmail.com>
2021-07-20 23:03:37 +0530

    Added: Ability To Delete Person

---
+++ src/deleteData.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+    public class delteData  {
+        public static <Contact> void main(String[] args) {
+            ArrayList<Contact> con=new ArrayList<Contact>();
+            System.out.println("Welcome to AddressBook Program");
+            Scanner sc= new Scanner(System.in);
+            System.out.println("Enter first NAme:");
+            String firstName=sc.next();
+            System.out.println("Enter Last Name:");
+            String lastName=sc.next();
+
+            System.out.println("Enter Address:");
+            String address=sc.next();
+
+            System.out.println("Enter City:");
+            String city=sc.next();
+
+            System.out.println("Enter State:");
+            String state=sc.next();
+
+            System.out.println("Enter Email:");
+            String email=sc.next();
+
+
+            System.out.println("Enter Mobile Number:");
+            String phoneNumber=sc.next();
+
+
+            Contact obj=new Contact(firstName,lastName,address,city,state,email,phoneNumber);
+            con.add(obj);
+            Iterator<Contact> itr=con.iterator();
+            while(itr.hasNext())
+            {
+                System.out.println(itr.next().toString());
+            }
+            //System.out.println("First name to modify the data:");
+            //String  name1=sc.next();
+            //int index = con.indexOf("name1");
+
+
+            System.out.println("Enter First name to delete the data:");
+            String name = sc.next();
+            con.remove(0);
+            System.out.println("After deleting"+con);
+        }
+
+    }
+
+
+
